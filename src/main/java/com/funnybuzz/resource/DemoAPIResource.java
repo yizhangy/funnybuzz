51ad2f23c0c07644ff71e1f89479413e1cbb3e97
Yizhang Yin <Yizhang.Yin@gmail.com>
2017-04-30 22:41:40 +1200

    simple demo REST setup

---
+++ src/main/java/com/funnybuzz/resource/DemoAPIResource.java
@@ -0,0 +1,37 @@
+/*************************************************************************
+ * 
+ * Forward Thinking CONFIDENTIAL
+ * __________________
+ * 
+ *  2013 - 2017 Forward Thinking Ltd
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Forward Thinking Ltd and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Forward Thinking Ltd
+ * and its suppliers and may be covered by New Zealand and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Forward Thinking Ltd.
+ */
+package com.funnybuzz.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/demo")
+public class DemoAPIResource{
+	
+	@GET
+	@Path("/test")
+	@Produces({ MediaType.TEXT_PLAIN })
+	public String test(){
+		return "OK";
+	}
+
+}
+
